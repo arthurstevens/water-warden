7496c139b1b3f893b84fbd37db0c52d2056ae43d
Custord <126922564+CustordSolos@users.noreply.github.com>
2025-06-10 13:42:19 +0100

    fix!: change table odeView to latestNodeView. - Retrieves all the correct fields. - Selects only the most recent logs per node. - BREAKING: External use cases need updating to consider this logic and name change.

--- DDL.sql
+++ DDL.sql
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS nodeLog (
     temperature REAL,
     turbidity REAL,
     totalDissolvedSolids REAL,
-    CONSTRAINT fk_nodeLog_nodeID FOREIGN KEY (nodeID) REFERENCES node(id)
+    CONSTRAINT fk_nodeLog_nodeID FOREIGN KEY (nodeID) REFERENCES node(id),
     PRIMARY KEY(nodeID, timestamp)
 );
 
@@ -80,7 +80,11 @@ INSERT INTO announcementLog (announcementID, userID, initialTime, expiry, create
 (2, 1, '2025-06-06 13:25:00', '2025-06-09 12:00:00', NOW());
 
 -- View
-CREATE OR REPLACE VIEW nodeView AS SELECT node.id, node.name, node.latitude, node.longitude, nodeLog.timestamp, nodeLog.flowRate, nodeLog.pressure, nodeLog.battery, nodeLog.temperature, nodeLog.turbidity, nodeLog.totalDissolvedSolids FROM node LEFT JOIN nodeLog ON node.id = nodeLog.nodeID;
+CREATE OR REPLACE VIEW latestNodeView AS 
+SELECT DISTINCT ON (nl.nodeID) nl.*, n.name, n.longitude, n.latitude
+FROM nodeLog nl
+JOIN node n ON n.ID = nl.nodeID
+ORDER BY nl.nodeID, nl.timestamp DESC;
 
 -- Test query
 SELECT announcementLog.announcementID, announcementLog.expiry, announcementPresets.heading, announcementPresets.content FROM announcementLog LEFT JOIN announcementPresets ON announcementPresets.announcementID=announcementLog.announcementID WHERE initialTime < NOW() AND expiry > NOW();
