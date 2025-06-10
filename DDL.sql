d862fa36d414d13a2929e3e39ad95e72a296be1b
Custord <126922564+CustordSolos@users.noreply.github.com>
2025-06-10 13:43:20 +0100

    style: move testing and seed to base of the DDL.

--- DDL.sql
+++ DDL.sql
@@ -67,6 +67,14 @@ CREATE TABLE IF NOT EXISTS announcementLog (
     CONSTRAINT fk_announcementLog_announcementID FOREIGN KEY (announcementID) REFERENCES announcementPresets(announcementID)
 );
 
+-- View
+CREATE OR REPLACE VIEW latestNodeView AS 
+SELECT DISTINCT ON (nl.nodeID) nl.*, n.name, n.longitude, n.latitude
+FROM nodeLog nl
+JOIN node n ON n.ID = nl.nodeID
+ORDER BY nl.nodeID, nl.timestamp DESC;
+
+-- Seed
 INSERT INTO node (name, latitude, longitude) VALUES
 ('Node001', 28.52955, 30.26594),
 ('Node002', 29.52955, 29.26594);
@@ -78,13 +86,6 @@ INSERT INTO announcementPresets (heading, content) VALUES
 INSERT INTO announcementLog (announcementID, userID, initialTime, expiry, createdDate) VALUES
 (1, 1, '2025-06-07 13:25:00', '2025-06-08 12:00:00', NOW()),
 (2, 1, '2025-06-06 13:25:00', '2025-06-09 12:00:00', NOW());
-
--- View
-CREATE OR REPLACE VIEW latestNodeView AS 
-SELECT DISTINCT ON (nl.nodeID) nl.*, n.name, n.longitude, n.latitude
-FROM nodeLog nl
-JOIN node n ON n.ID = nl.nodeID
-ORDER BY nl.nodeID, nl.timestamp DESC;
 
 -- Test query
 SELECT announcementLog.announcementID, announcementLog.expiry, announcementPresets.heading, announcementPresets.content FROM announcementLog LEFT JOIN announcementPresets ON announcementPresets.announcementID=announcementLog.announcementID WHERE initialTime < NOW() AND expiry > NOW();
